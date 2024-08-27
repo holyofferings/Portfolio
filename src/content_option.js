a06c256065255c21a605d930b8943ecb8416e608
Nikhil <nikhil@Nikhils-MacBook-Air.local>
2024-08-27 12:53:29 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "Nikhil Prasad";
+const meta = {
+    title: "Nikhil Prasad",
+    description: "I'm Nikhil Prasad, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hi, I'm Nikhil Prasad",
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Cyber Security Learner",
+        fourth: "Prompt Engineer",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/M9V1wGH/Nikhil.png",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I am Nikhil Prasad, a Results‑driven Prompt Engineer with a strong foundation in AI research and development, and a keen interest in Cloud Security Engineering. Proven track record in enhancing user engagement and response rates through innovative prompt engineering and fine‑tuning large language models. Demonstrated expertise in integrating industry‑specific terminology and performance monitoring to drive strategic decision‑making. Skilled in utilizing industry‑leading APIs and building AI‑powered applications. Possesses robust technical, security, and soft skills, with a strong academic background in Computer Science. Recognized for outstanding project work and contributions to AI and machine learning. Eager to leverage expertise in AI to further advance in cloud security engineering.",
+};
+const worktimeline = [{
+        jobtitle: "Prompt Engineer",
+        where: "SOUL AI",
+        date: "March,24 - May,24",
+    },
+    {
+        jobtitle: "AI R&D Intern",
+        where: "Adeptia",
+        date: "Dec,23 - Mar,24",
+    },
+    {
+        jobtitle: "Prompt Engineer",
+        where: "Freelancer",
+        date: "Mar,23 - Present",
+    },
+];
+
+const skills = [{
+        name: "Python",
+    },
+    
+    {
+        name: "Prompt Engineering",
+    },
+    
+    {
+        name: "Gen AI Tools",
+    },
+    
+    {
+        name: "AWS Cloud",
+    },
+
+    {
+        name: "Linxu, MacOS",
+    },
+
+    {
+        name: "Splunk, Wireshark ",
+    },
+
+    {
+        name: "Langchain",
+    },
+
+];
+
+const dataportfolio = [{
+    img: "https://i.ibb.co/42qQTMk/File-Loading.jpg",
+    description: "A conversational AI model to ask questions about the content of uploaded PDF files.",
+    link: "https://github.com/holyofferings/Chat_with_pdf_using_GeminiAPI",
+},
+    {
+        img: "https://i.ibb.co/RTFG05t/Main-Screen.jpg",
+        description: "GENERATES NEW AND SECURED PASSWORD FOR DIFFERENT WEBSITES. ",
+        link: "https://github.com/holyofferings/Password-Manager",
+    },
+    {
+        img: "https://i.ibb.co/vVYYK39/Prompt-Engineering-4.jpg",
+        description: "This guide is being developed to provide comprehensive insights into prompt engineering",
+        link: "https://github.com/holyofferings/Prompt-Engineering-Guide/tree/main",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "holyofferings22@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_m3mg6v2",
+    YOUR_TEMPLATE_ID: "template_67qh2b7",
+    YOUR_USER_ID: "roAR7USV_ipIq0q0I",
+};
+
+const socialprofils = {
+    github: "https://github.com/holyofferings",
+    linkedin: "https://www.linkedin.com/in/nikhil-prasad-2192501b7/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
